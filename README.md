fba5c2e6f8034b24bf4f4367ff650daba804c8c7
krmilan <109038886+krmilan@users.noreply.github.com>
2023-12-17 20:43:52 +0530

    first commit - complete weather application using OpenWeather API

---
+++ README.md
@@ -0,0 +1,1 @@
+# Geo-Weather Application | Capstone Project - Use a Public API
---
+++ index.js
@@ -0,0 +1,51 @@
+import express from "express";
+import axios from "axios";
+import bodyParser from "body-parser";
+
+const app = express();
+const port = 3000;
+
+app.use(bodyParser.urlencoded({extended:true}));
+app.use(express.static("public"));
+
+app.get("/", async (req, res)=>{
+try{
+    const response = await axios.get("https://api.openweathermap.org/data/2.5/weather?q=London&units=metric&appid=b21dc7d3ebeea2db03d40068467132d9");
+    const result = response.data;
+    console.log(result);
+    res.render("index.ejs",{
+        data: result,
+    });
+}catch(error){
+    console.error("Failed to make request:", error.message);
+    res.render("index.ejs", {
+      error: "Failed to make request.",
+    });
+}
+})
+
+app.post("/", async(req, res)=>{
+    try{
+        const city = req.body.city;
+        const response = await axios.get(`https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=b21dc7d3ebeea2db03d40068467132d9`);
+        const result = response.data;
+        console.log(result);
+        res.render("index.ejs",{
+            data : result,
+        });
+    }catch(error){
+        console.error("Failed to make request:", error.message);
+        res.render("index.ejs", {
+          error: "No city found.",
+        });
+    }
+});
+
+
+
+
+
+
+app.listen(port, ()=>{
+    console.log(`Server running on port ${port}`);
+});
